48d4c86cb5761e7f2aa61773b6f01ec0963558ad
rjbasitali <rjbasitali@gmail.com>
2023-08-08 12:51:52 +0500

    db conn config

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"time"
 
 	"git.rjbasitali.com/at-case-study/pkg/models"
 	"github.com/gin-gonic/gin"
@@ -14,6 +15,7 @@ var DB *gorm.DB
 // Init initializes the database connection.
 // It accepts the connection string as a parameter.
 // It panics if the connection to the database could not be established.
+// It also sets the maximum idle connections, maximum open connections and connection max lifetime.
 func Init(connStr string) {
 	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
 
@@ -23,5 +25,23 @@ func Init(connStr string) {
 	}
 
 	db.AutoMigrate(&models.Product{})
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		fmt.Fprintln(gin.DefaultErrorWriter, "error getting database connection: ", err)
+		panic("failed to get database connection")
+	}
+
+	err = sqlDB.Ping()
+	if err != nil {
+		fmt.Fprintln(gin.DefaultErrorWriter, "error pinging database: ", err)
+		panic("failed to ping database")
+	}
+	fmt.Fprintln(gin.DefaultWriter, "Connected to database")
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(25)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
 	DB = db
 }
